7bf1087a6d63cc71f7b0bf698f219f25fa8af27f
Nick Reuter <nreuter85@gmail.com>
2016-08-25 00:08:56 -0500

    audio: use kernel headers where possible
    Change-Id: Ia85a58ac06dc687df7d4606e125d211decee8dd0

--- msm8909/post_proc/Android.mk
+++ msm8909/post_proc/Android.mk
@@ -30,7 +30,7 @@ LOCAL_MODULE:= libqcompostprocbundle
 
 LOCAL_C_INCLUDES := \
 	external/tinyalsa/include \
-	$(BOARD_KERNEL_HEADER_DIR) \
+	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
 	$(call include-path-for, audio-effects)
 
 include $(BUILD_SHARED_LIBRARY)
